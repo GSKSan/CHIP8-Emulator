04ab2350787d1f84ea4425a8ede01e67147290c4
GSKSan <sksanthoshg94@gmail.com>
2019-08-28 20:17:02 +1200

    Add project files.

---
+++ Chip8/CPU.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chip8
+{
+    class CPU
+    {
+        //Memory
+        public byte[] memory = new byte[4096];
+
+        //Registers
+        public byte[] registers = new byte[16];
+
+        //Address register
+        public ushort I = 0;
+
+        //stack
+        public Stack<ushort> stack = new Stack<ushort>();
+
+        //Timers
+        public byte DelayTimer;
+        public byte SoundTimer;
+
+        //Keyboard
+        public byte Keyboard;
+
+        //Display
+        public byte[] Display = new byte[64 * 32];
+
+        public void opCodesExecution(ushort opcode)
+        {
+            ushort nibble = (ushort)(opcode & 0xF000);
+
+            switch(nibble)
+            {
+                case 0x0000:
+                    if(opcode == 0x00e0)
+                    {
+                        for (int i = 0; i < Display.Length; i++)
+                        {
+                            Display[i] = 0;
+                        }
+                    }
+                    else if(opcode == 0x00ee)
+                    {
+                        I = stack.Pop();
+                    }
+                    else
+                    {
+                        throw new Exception("Opcpde note supported "+opcode.ToString("X4"));
+                    }
+                    break;
+                default:
+                    throw new Exception($"Opcode not supported " + opcode.ToString("X4"));
+
+            }
+        }
+
+
+    }
+}
---
+++ Chip8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Chip8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CPU cpu = new CPU();
+            using (BinaryReader reader = new BinaryReader(new FileStream(@"C:\Users\OEM\Desktop\Ch8\IBM Logo.ch8", FileMode.Open)))
+            {
+                while (reader.BaseStream.Position < reader.BaseStream.Length)
+                {
+                    var opcode =(ushort) (reader.ReadByte() << 8 | reader.ReadByte());
+
+                    // Console.WriteLine(opcode.ToString("X4"));
+                    try
+                    {
+                        cpu.opCodesExecution(opcode);
+
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
